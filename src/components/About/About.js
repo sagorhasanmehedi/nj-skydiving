8551b4faa259a0caaf3bfca50799f4387539fc7b
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2021-11-27 14:32:44 +0600

    remove heading

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,6 @@ const About = () => {
           their <br /> own professionalism.
         </h1>
         <button onClick={websitedubai}>READ MORE </button>
-        <h1>i am chaking right now</h1>
       </div>
     </div>
   );
